4a22050dc59dcc30381d9706ab3d30e74605a3d9
RiouC <riou.christophe@protonmail.com>
2021-04-12 16:50:14 +0200

    [Gradient] add tags in props, update GradientsList

--- src/components/Gradient.js
+++ src/components/Gradient.js
@@ -1,14 +1,18 @@
 import GradientTitle from "./GradientTitle"
 import GradientCode from "./GradientCode"
 import GradientPill from "./GradientPill"
+import { gradient } from "../gradients"
 
-const Gradient = ({ colorStart, colorEnd, name }) => {
+const Gradient = ({ colorStart, colorEnd, name, tags }) => {
   return (
     <li className="col-lg-3 col-md-4 col-sm-6">
       <div className="card p-3 mb-4 shadow">
         <GradientPill colorStart={colorStart} colorEnd={colorEnd} />
         <GradientTitle>{name}</GradientTitle>
         <GradientCode colorStart={colorStart} colorEnd={colorEnd} />
+        <div>
+          {tags.map(tag => <button className="btn border bg-dark text-white mx-1 mt-2">{tag}</button>)}
+        </div>
       </div>
     </li>
   )
