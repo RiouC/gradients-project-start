cd54e7531e70485c307015f1a22b38795ee4f8f4
RiouC <riou.christophe@protonmail.com>
2021-04-12 18:40:57 +0200

    [GradientsSelect] add label

--- src/components/GradientsSelect.js
+++ src/components/GradientsSelect.js
@@ -2,10 +2,13 @@ import { uniqueTags } from "../gradients"
 
 const GradientsSelect = () => {
     return (
-        <select className="form-control mb-4">
-            <option value="Tous">Tous</option>
-            {uniqueTags.map(tag => <option value={tag}>{tag}</option>)}
-        </select>
+        <form className="input-group mb-3">
+            <label className="input-group-text" for="select">Filtrer par tag</label>
+            <select id="select" className="form-select">
+                <option value="Tous">Tous</option>
+                {uniqueTags.map(tag => <option value={tag}>{tag}</option>)}
+            </select>
+        </form>
     )
 }
 
