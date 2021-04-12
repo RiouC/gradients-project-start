02c361ad45306d752d8fc573dc8927a94653780e
RiouC <riou.christophe@protonmail.com>
2021-04-12 15:47:29 +0200

    [GradientsList] add key

--- src/components/GradientsList.js
+++ src/components/GradientsList.js
@@ -5,7 +5,7 @@ import { gradients } from "../gradients"
 const GradientsList = () => {
   return (
     <ul className="row list-unstyled">
-      {gradients.map(grad => <Gradient colorStart={grad.start} colorEnd={grad.end} name={grad.name} />)
+      {gradients.map(grad => <Gradient key={grad.name} colorStart={grad.start} colorEnd={grad.end} name={grad.name} />)
       }
     </ul>
   )
