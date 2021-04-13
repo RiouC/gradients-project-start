b4ed879c3e7a7bfdd687ecd0a9c52289c208ef00
RiouC <riou.christophe@protonmail.com>
2021-04-13 12:14:26 +0200

    [Footer] class -> className

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 
 const Footer = () => {
     return (
-        <footer class="bg-dark text-white text-center mt-auto p-3">
+        <footer className="bg-dark text-white text-center mt-auto p-3">
             <span lang="en">Made with Love for Alyra</span>
         </footer>
     );
