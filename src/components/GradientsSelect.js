ca065df76575ab5c6e590a536e7ef707551444f4
RiouC <riou.christophe@protonmail.com>
2021-04-13 09:56:03 +0200

    [GradientSelect] update GradientSelect

--- src/components/GradientsSelect.js
+++ src/components/GradientsSelect.js
@@ -18,26 +18,27 @@
 
 import { uniqueTags } from "../gradients";
 
+
 const GradientsSelect = (props) => {
-  const { filterGradients } = props;
-  return (
-    <form className="input-group mb-3">
-      <label className="input-group-text" htmlFor="select">
-        Filtrer par tag
-      </label>
-      <select
-        id="select"
-        className="form-select"
-        onChange={(event) => filterGradients(event.target.value)}
-      >
-        <option value="Tous">Tous</option>
-        {uniqueTags.map((tag) => (
-          <option value={tag}>{tag}</option>
-        ))}
-      </select>
-    </form>
-  );
-};
+    const { tag, setTag } = props;
+    const handleSelectChange = (event) => {
+        setTag(event.target.value)
+    }
+    return (
+        <form className="input-group mb-3">
+            <label className="input-group-text" htmlFor="select">Filtrer par tag</label>
+            <select
+                id="select"
+                className="form-select"
+                onChange={handleSelectChange}
+                selected={tag}
+            >
+                <option value="all">Tous</option>
+                {uniqueTags.map(tag => <option value={tag}>{tag}</option>)}
+            </select>
+        </form>
+    )
+}
 
 export default GradientsSelect;
 
