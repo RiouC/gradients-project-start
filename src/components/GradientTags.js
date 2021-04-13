fed662097c1400e5b073e519ccfc4db2cb686b24
RiouC <riou.christophe@protonmail.com>
2021-04-13 12:12:48 +0200

    [GradientsTags] tagsClasses is now a function

--- src/components/GradientTags.js
+++ src/components/GradientTags.js
@@ -1,17 +1,18 @@
 const GradientTags = ({ tags, tag, setTag }) => {
-  const tagsClasses = tag === tags ? "bg-light btn border mx-1 mt-2" : "bg-dark text-white btn border mx-1 mt-2";
 
   const handleButtonClick = (event) => {
     console.log(event.target.value)
     setTag(event.target.value);
   };
 
-  //  const tagsClasses = () => {
-  //  }
+  const tagsClasses = (el) => {
+    return tag === el ? "bg-light btn border mx-1 mt-2" : "bg-dark text-white btn border mx-1 mt-2";
+  }
+
   return (
     <div>
       {tags.map((el) => {
-        return <button key={el} disabled={el === tag} className={tagsClasses} value={el} onClick={handleButtonClick}>{el}</button>
+        return <button key={el} disabled={el === tag} className={tagsClasses(el)} value={el} onClick={handleButtonClick}>{el}</button>
       })}
     </div>
   )
