eef205f8a0f3878c99aa9c755aa20b9fd7d4dc13
RiouC <riou.christophe@protonmail.com>
2021-04-13 13:02:47 +0200

    [GradientTags] white background, margin-top

--- src/components/GradientTags.js
+++ src/components/GradientTags.js
@@ -6,11 +6,11 @@ const GradientTags = ({ tags, tag, setTag }) => {
   };
 
   const tagsClasses = (el) => {
-    return tag === el ? "bg-light btn border mx-1 mt-2" : "bg-dark text-white btn border mx-1 mt-2";
+    return tag === el ? "btn btn-sm me-2 mb-2 bg-light" : "btn btn-sm me-2 mb-2 bg-dark text-white";
   }
 
   return (
-    <div>
+    <div className="mt-3">
       {tags.map((el) => {
         return <button key={el} disabled={el === tag} className={tagsClasses(el)} value={el} onClick={handleButtonClick}>{el}</button>
       })}
